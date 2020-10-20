8ac11c1e4075ab96cebdcd607b1ac203c4c4438b
Victor <larrezetvictor@gmail.com>
2020-10-20 10:39:24 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  
+    render() {
+        return (
+            <div>
+                <main>
+                    <div className="ctn-main">
+                        <div className="ctn-part-1">
+                            <div>
+                                <div className="css-zbbntt-Divider svg-ctn-part-1">
+                                    <div className="css-q42gqc-UpDown-upDownAnimation e1l90hg90">
+                                        <svg viewBox="0 0 30 30" class="css-1l5zof9-SVG"><polygon stroke-width="1px" stroke-linejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon></svg>
+                                        <svg viewBox="0 0 30 30" class="css-e3v4f3-SVG"><polygon stroke-linejoin="round" stroke-miterlimit="10" points="27.5,21.904 15,28.809  2.5,21.904 2.5,8.095 15,1.19 27.5,8.095 "></polygon></svg>
+                                        <svg viewBox="0 0 30 30" class="css-1ov6vuz-SVG"><path d="M28,2V28H2V2H28m.13-2H1.88A1.88,1.88,0,0,0,0,1.88V28.13A1.88,1.88,0,0,0,1.88,30H28.13A1.87,1.87,0,0,0,30,28.13V1.88A1.88,1.88,0,0,0,28.13,0Z"/></svg>
+                                    </div>
+                                    <div class="css-1hsk6m9-UpDownWide-upDownWideAnimation e1l90hg91">
+                                        <svg viewBox="0 0 30 42" class="css-lg1nd6-SVG"><path d="M28.74,20.81H1.26a1.26,1.26,0,0,1-1-2L14.16.5a1.25,1.25,0,0,1,1-.5h0a1.24,1.24,0,0,1,1,.51L29.75,18.8a1.25,1.25,0,0,1-1,2ZM3.81,18.29H26.22L15.16,3.37Z"></path> <path d="M28.74,42H1.26a1.28,1.28,0,0,1-1.13-.71A1.26,1.26,0,0,1,.26,40l13.9-18.29a1.28,1.28,0,0,1,1-.5h0a1.24,1.24,0,0,1,1,.51L29.75,40a1.26,1.26,0,0,1,.12,1.32A1.28,1.28,0,0,1,28.74,42ZM3.81,39.47H26.22L15.16,24.55Z"></path></svg>
+                                        <svg viewBox="0 0 30 30" class="css-k8zaxw-SVG"><polygon stroke-width="1px" stroke-linejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon></svg>
+                                        <svg viewBox="0 0 30 30" class="css-1jd6e1k-SVG"><path d="M15,30A15,15,0,1,1,30,15,15,15,0,0,1,15,30ZM15,6.23A8.77,8.77,0,1,0,23.77,15,8.77,8.77,0,0,0,15,6.23Z"></path></svg>
+                                        <svg viewBox="0 0 30 30" class="css-1twtfa1-SVG"><polygon stroke-width="1px" stroke-linejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon></svg>
+                                        <svg viewBox="0 0 100 100" class="css-1vae1u5-SVG"><path stroke-width="3px" d="M60.5,50l34.8-34.8c2.9-2.9,2.9-7.6,0-10.5c-2.9-2.9-7.6-2.9-10.5,0L50,39.5L15.2,4.7c-2.9-2.9-7.6-2.9-10.5,0    c-2.9,2.9-2.9,7.6,0,10.5L39.5,50L4.7,84.8c-2.9,2.9-2.9,7.6,0,10.5c1.4,1.4,3.3,2.2,5.2,2.2s3.8-0.7,5.2-2.2L50,60.5l34.8,34.8    c1.4,1.4,3.3,2.2,5.2,2.2c1.9,0,3.8-0.7,5.2-2.2c2.9-2.9,2.9-7.6,0-10.5L60.5,50z"></path></svg>
+                                        <svg viewBox="0 0 30 30" class="css-8o6gnp-SVG"><path d="M15,30A15,15,0,1,1,30,15,15,15,0,0,1,15,30ZM15,6.23A8.77,8.77,0,1,0,23.77,15,8.77,8.77,0,0,0,15,6.23Z"></path></svg>
+                                        <svg viewBox="0 0 30 44.58" class="css-4y1k1v-SVG"><path d="M28.74,44.58a1.28,1.28,0,0,1-1-.51L15.16,27.13l-12.89,17a1.26,1.26,0,1,1-2-1.53l13.9-18.29a1.34,1.34,0,0,1,1-.5,1.24,1.24,0,0,1,1,.51L29.75,42.56a1.27,1.27,0,0,1-1,2Z"></path><path d="M14.83,20.82h0a1.28,1.28,0,0,1-1-.52L.25,2a1.27,1.27,0,0,1,2-1.51L14.84,17.45,27.73.5a1.26,1.26,0,0,1,2,1.53L15.84,20.32A1.28,1.28,0,0,1,14.83,20.82Z"></path></svg>
+                                    </div>
+                                    <svg viewBox="0 0 30 30" class="css-1rqrcoz-SVG"><path d="M15,30A15,15,0,1,1,30,15,15,15,0,0,1,15,30ZM15,6.23A8.77,8.77,0,1,0,23.77,15,8.77,8.77,0,0,0,15,6.23Z"></path></svg>
+                                    <svg viewBox="0 0 30 30" class="css-zn506t-SVG"><path d="M15,30A15,15,0,1,1,30,15,15,15,0,0,1,15,30ZM15,6.23A8.77,8.77,0,1,0,23.77,15,8.77,8.77,0,0,0,15,6.23Z"></path></svg>
+                                    <svg viewBox="0 0 30 30" class="css-jyibgf-SVG"><path d="M15,30A15,15,0,1,1,30,15,15,15,0,0,1,15,30ZM15,6.23A8.77,8.77,0,1,0,23.77,15,8.77,8.77,0,0,0,15,6.23Z"></path></svg>
+                                    <svg viewBox="0 0 30 44.58" class="css-1jc5w51-SVG"><path d="M28.74,44.58a1.28,1.28,0,0,1-1-.51L15.16,27.13l-12.89,17a1.26,1.26,0,1,1-2-1.53l13.9-18.29a1.34,1.34,0,0,1,1-.5,1.24,1.24,0,0,1,1,.51L29.75,42.56a1.27,1.27,0,0,1-1,2Z"></path><path d="M14.83,20.82h0a1.28,1.28,0,0,1-1-.52L.25,2a1.27,1.27,0,0,1,2-1.51L14.84,17.45,27.73.5a1.26,1.26,0,0,1,2,1.53L15.84,20.32A1.28,1.28,0,0,1,14.83,20.82Z"></path></svg>
+                                    <svg viewBox="0 0 30 44.58" class="css-wfgihl-SVG"><path d="M28.74,44.58a1.28,1.28,0,0,1-1-.51L15.16,27.13l-12.89,17a1.26,1.26,0,1,1-2-1.53l13.9-18.29a1.34,1.34,0,0,1,1-.5,1.24,1.24,0,0,1,1,.51L29.75,42.56a1.27,1.27,0,0,1-1,2Z"></path><path d="M14.83,20.82h0a1.28,1.28,0,0,1-1-.52L.25,2a1.27,1.27,0,0,1,2-1.51L14.84,17.45,27.73.5a1.26,1.26,0,0,1,2,1.53L15.84,20.32A1.28,1.28,0,0,1,14.83,20.82Z"></path></svg>
+                                    <svg viewBox="0 0 30 30" class="css-1u47rbk-SVG"><polygon stroke-width="1px" stroke-linejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon></svg>
+                                    <svg viewBox="0 0 30 30" class="css-1ah40zy-SVG"><path d="M15,30A15,15,0,1,1,30,15,15,15,0,0,1,15,30ZM15,6.23A8.77,8.77,0,1,0,23.77,15,8.77,8.77,0,0,0,15,6.23Z"></path></svg>
+                                    <svg viewBox="0 0 30 30" class="css-kh7ws0-SVG"><path d="M28,2V28H2V2H28m.13-2H1.88A1.88,1.88,0,0,0,0,1.88V28.13A1.88,1.88,0,0,0,1.88,30H28.13A1.87,1.87,0,0,0,30,28.13V1.88A1.88,1.88,0,0,0,28.13,0Z"></path></svg>
+                                    <svg viewBox="0 0 30 30" class="css-19fynh1-SVG"><path d="M28,2V28H2V2H28m.13-2H1.88A1.88,1.88,0,0,0,0,1.88V28.13A1.88,1.88,0,0,0,1.88,30H28.13A1.87,1.87,0,0,0,30,28.13V1.88A1.88,1.88,0,0,0,28.13,0Z"></path></svg>
+                                    <svg viewBox="0 0 30 30" class="css-ztgfeg-SVG"><path d="M28,2V28H2V2H28m.13-2H1.88A1.88,1.88,0,0,0,0,1.88V28.13A1.88,1.88,0,0,0,1.88,30H28.13A1.87,1.87,0,0,0,30,28.13V1.88A1.88,1.88,0,0,0,28.13,0Z"></path></svg>
+                                    <svg viewBox="0 0 30 30" class="css-139zj92-SVG"><polygon stroke-linejoin="round" stroke-miterlimit="10" points="27.5,21.904 15,28.809  2.5,21.904 2.5,8.095 15,1.19 27.5,8.095 "></polygon></svg>
+                                    <svg viewBox="0 0 30 30" class="css-1ahfg5i-SVG"><polygon stroke-linejoin="round" stroke-miterlimit="10" points="27.5,21.904 15,28.809  2.5,21.904 2.5,8.095 15,1.19 27.5,8.095 "></polygon></svg>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+            )
+      }
+}
+
+export default App;

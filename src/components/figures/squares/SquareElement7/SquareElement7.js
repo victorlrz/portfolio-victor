0f82d00cf75be85b7a9ae7dc58ad4b9aab4292da
Victor <larrezetvictor@gmail.com>
2020-10-21 12:04:25 +0200

    :construction: building new components

---
+++ src/components/figures/squares/SquareElement7/SquareElement7.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Style from "./SquareElement7.module.scss";
+
+export default class SquareElement7 extends Component {
+  render() {
+    return (
+        <svg viewBox="0 0 30 30" className={Style.cssSquareElement7SVG}><path d="M28,2V28H2V2H28m.13-2H1.88A1.88,1.88,0,0,0,0,1.88V28.13A1.88,1.88,0,0,0,1.88,30H28.13A1.87,1.87,0,0,0,30,28.13V1.88A1.88,1.88,0,0,0,28.13,0Z"></path></svg>
+    );
+  }
+}

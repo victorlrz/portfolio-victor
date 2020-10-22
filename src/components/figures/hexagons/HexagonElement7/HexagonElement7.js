de391b2dcd7a6c8337c2ed8928d32ed8492e8519
Victor <larrezetvictor@gmail.com>
2020-10-22 14:44:54 +0200

    :fire: section4 complete

---
+++ src/components/figures/hexagons/HexagonElement7/HexagonElement7.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Style from "./HexagonElement7.module.scss";
+
+export default class HexagonElement7 extends Component {
+  render() {
+    return (
+        <svg viewBox="0 0 30 30" className={Style.cssHexagonElement7SVG}><polygon stroke-linejoin="round" stroke-miterlimit="10" points="27.5,21.904 15,28.809  2.5,21.904 2.5,8.095 15,1.19 27.5,8.095 "></polygon></svg>
+    );
+  }
+}
+
--- src/components/sections/Section4/Section4.js
+++ src/components/sections/Section4/Section4.js
@@ -5,6 +5,9 @@ import ContactContent from "../../section4-details/ContactContent/ContactContent
 import Divider from "../../section4-details/Divider/Divider"
 import AnimationUpDownWide4 from "../../section4-details/AnimationUpDowWide/AnimationUpWide4";
 import CircleElement16 from "../../figures/circles/CircleElement16/CircleElement16";
+import CircleElement17 from "../../figures/circles/CircleElement17/CircleElement17";
+import SquareElement11 from "../../figures/squares/SquareElement11/SquareElement11";
+import HexagonElement7 from "../../figures/hexagons/HexagonElement7/HexagonElement7";
 export default class Section4 extends Component {
   render() {
     return (
@@ -15,6 +18,9 @@ export default class Section4 extends Component {
             <AnimationUpDown4 />
             <AnimationUpDownWide4 />
             <CircleElement16 />
+            <CircleElement17 />
+            <SquareElement11 />
+            <HexagonElement7 />
             </div>
         </div>
     );
